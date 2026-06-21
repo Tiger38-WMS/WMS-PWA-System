e5363917a2eb27361ad1d7eaaded172fa0506a5a
Tiger38-WMS <wenyuan1405@gmail.com>
2026-06-21 21:37:08 +0700

    Thay đổi APP_NAME trong file config.js sang tiếng Việt

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.WMS_CONFIG = {
   // Dán URL Google Apps Script Web App /exec của bạn vào đây.
   GAS_API_URL: 'https://script.google.com/macros/s/AKfycbwMiQDunhWuO1dqaB2HtcH7scCGhSYpcDnetMG8VRSPHge-2d6WYdny-IAtyhAEkodW/exec',
-  APP_NAME: 'WMS PWA',
+  APP_NAME: 'Quản lý kho WMS',
   SESSION_KEY: 'wms.session.v1',
   DEVICE_KEY: 'wms.device.v1',
   API_TIMEOUT_MS: 25000,
